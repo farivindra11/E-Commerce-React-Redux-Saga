ed42b69dcbe69987b0115e6e44636e0ec5abb49e
fariv indra <farivind@gmail.com>
2020-09-24 00:18:55 +0700

    api done

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models/index');
 const path = require('path');
-const API_SERVER="http://localhost:3001";
+const API_SERVER = "http://localhost:3001";
 
 
 /* GET home page. */
@@ -13,30 +13,30 @@ router.get('/', async (req, res, next) => {
     const offset = page * limit - limit
 
     const dataProducts = await models.Products.findAndCountAll({
-        order: [
-            ['createdAt', 'ASC']
-        ],
-        limit,
-        offset
+      order: [
+        ['createdAt', 'ASC']
+      ],
+      limit,
+      offset
     })
 
     const totalPage = Math.ceil(dataProducts.count / limit)
     const response = {
-        totalPage,
-        data:dataProducts.rows
+      totalPage,
+      data: dataProducts.rows
     }
-    response.test='test'
+    response.test = 'test'
     res.json(response)
 
-} catch (error) {
+  } catch (error) {
     console.log(error)
     res.status(500).json(error)
-}
+  }
 });
 
 /* find product details */
 router.get('/:id', async (req, res) => {
-  try{
+  try {
     const catalog = await models.Products.findByPk(req.params.id)
     res.json(catalog);
   } catch (error) {
@@ -45,49 +45,66 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  try{
+  try {
     const { title, rate, description, price, brand, detailProduct, capacities } = req.body;
 
     const file = req.files.image
-        const fileName = file.name.toLowerCase().replace("", Date.now()).split(' ').join('-')
-        await file.mv(path.join(__dirname, "..", "public", "images", fileName))
+    const fileName = file.name.toLowerCase().replace("", Date.now()).split(' ').join('-')
+    await file.mv(path.join(__dirname, "..", "public", "images", fileName))
 
-        const catalog = await models.Products.create ({
-          title,
-          rate: Number(rate),
-          description,
-          price,
-          brand,
-          detail_product: detailProduct,
-          image: `${API_SERVER}/images/${fileName}`,
-          capacities: capacities.split(',')
-        })
-        res.json(catalog)
+    const catalog = await models.Products.create({
+      title,
+      rate: Number(rate),
+      description,
+      price,
+      brand,
+      detail_product: detailProduct,
+      image: `${API_SERVER}/images/${fileName}`,
+      capacities: capacities.split(',')
+    })
+    res.json(catalog)
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
   }
 });
 
-// Delete
+/* Delete */
 router.delete('/:id', async (req, res) => {
   try {
-      const delProducts = await models.Products.findByPk(req.params.id)
-      if (delProducts) {
-          await models.Products.destroy({
-              where: {
-                  id: req.params.id
-              }
-          })
-          res.json(delProducts);
-      }
+    const delProducts = await models.Products.findByPk(req.params.id)
+    if (delProducts) {
+      await models.Products.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      res.json(delProducts);
+    }
 
   } catch (error) {
-      console.log(error)
-      res.status(500).json(error)
+    console.log(error)
+    res.status(500).json(error)
   }
 });
 
+/* Edit */
+router.put('/:id', async (req, res) => {
+  try {
+    const catalog = await models.Products.update({
+      vote: req.body.vote,
 
+    }, {
+      returning: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(catalog)
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
+})
 
 module.exports = router;
