4d85d682c078bb6bbab97659df56ee9c71932a17
fariv indra <farivind@gmail.com>
2020-09-25 03:52:33 +0700

    detail reducer done

---
+++ client/src/reducers/detailAdd.js
@@ -0,0 +1,27 @@
+import numToRupiah from '../helpers/rupiah';
+
+const detaiAdd = (state = {}, action) => {
+    switch (action.type) {
+        case 'LOAD_DETAIL_ADD' :
+        const data = action.data
+        return {
+            ...data,
+            price:numToRupiah(data.price)
+        }
+        case 'RESET_DETAIL_ADD' :
+            return {}
+        case 'UPVOTE_ADD':
+            return {
+                ...state,
+                vote: state.vote + 1
+            }
+        case 'DEVOTE_ADD':
+            return {
+                ...state,
+                vote: state.vote - 1
+            }
+        default:
+            return state
+    }
+} 
+export default detailAdd
