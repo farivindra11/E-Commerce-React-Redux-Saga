2ab43c3f30bc0021434cc12f7b5182d3328ab497
fariv indra <farivind@gmail.com>
2020-09-25 03:47:26 +0700

    first commit

---
+++ client/src/containers/ProductBox.js
@@ -0,0 +1,6 @@
+import React, { Conponent } from 'react';
+import ProductItem from '../components/ProductItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import connect from 'react-redux';
+
+
