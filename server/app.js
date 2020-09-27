d818381ed61ba6abcddc3ad53aef89c7727e2a6f
fariv indra <farivind@gmail.com>
2020-09-27 13:42:00 +0700

    add cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
+const cors = require('cors')
 
 var productsRouter = require('./routes/products');
 var usersRouter = require('./routes/users');
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload({ limits: { fileSize: 4 * 1024 * 1024 } }))
+app.use(cors());
 
 app.use('/api/product', productsRouter);
 app.use('/users', usersRouter);
